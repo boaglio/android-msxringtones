333e52bc8b2311eb2b3e6fe6a9e8f1973069751c
Fernando Boaglio <boaglio@gmail.com>
2024-07-16 00:18:19 -0300

    version 10 - new policy

--- app/src/main/java/com/olimposystems/android/SobreActivity.java
+++ app/src/main/java/com/olimposystems/android/SobreActivity.java
@@ -2,6 +2,7 @@ package com.olimposystems.android;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,24 +11,20 @@ public class SobreActivity extends Activity {
 
 	protected void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
-		
+
 		setContentView(R.layout.sobre);
-		
+
 		Button botao1 = (Button) findViewById(R.id.botao_comments);
 		botao1.setOnClickListener(new View.OnClickListener() {
-			
+
 			public void onClick(View v) {
-				
-				Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);  
-				  
-				emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"contato@olymposystems.com"}); 
-				emailIntent.putExtra(Intent.EXTRA_SUBJECT, "comments about MSX ringtone"); 
-				emailIntent.setType("plain/text");  
-				  
-				startActivity(emailIntent); 
-				
+
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://boaglio.com/index.php/en/privacy-policy/"));
+
+				startActivity(browserIntent);
+
 			}
 		});
 	}
-		
+
 }
